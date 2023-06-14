03f5858703f4430cb8420a27be0b7aa3d6f96f92
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2023-06-14 13:19:21 +0300

    updating view, controller

--- js/controller.js
+++ js/controller.js
@@ -2,5 +2,11 @@ export default class Controller {
     constructor(model, view) {
         this.model = model
         this.view = view
+
+        this.onTodoListChanged(this.model.todos)
+    }
+
+    onTodoListChanged = (todos) => {
+        this.view.displayTodos(todos)
     }
 }
