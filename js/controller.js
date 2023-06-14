3cf633ffd46dfddddb317bd559a6315529db4136
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2023-06-14 17:02:42 +0300

    updating MVC

--- js/controller.js
+++ js/controller.js
@@ -3,10 +3,34 @@ export default class Controller {
         this.model = model
         this.view = view
 
+        this.view.bindDeleteTodo(this.handleDeleteTodo)
+        this.view.bindToggleTodo(this.handleToggleTodo)
+        this.view.bindAddTodo(this.handleAddTodo)
+
+
         this.onTodoListChanged(this.model.todos)
+        this.model.bindTodoListChanged(this.onTodoListChanged)
+
+        this.view.bindEditTodo(this.handleEditTodo)
     }
+
+    
 
     onTodoListChanged = (todos) => {
         this.view.displayTodos(todos)
+    }    
+
+    handleAddTodo = (todoText) => {
+        this.model.addTodo(todoText)
     }
+
+    handleEditTodo = (id, todoText) => {
+        this.model.editTodo(id, todoText)
+    }
+
+    handleToggleTodo = (id) => {
+        this.model.toggleTodo(id)
+    }
+
+    
 }
--- js/model.js
+++ js/model.js
@@ -1,36 +1,50 @@
 export default class Model {
     constructor() {
-        this.todos = [
-            {id: 1, text: 'Run a marathon', complete: false},
-            {id: 2, text: 'Plant a garden', complete: false}
-        ]
-    }
-
-    addTodo(todoText) {
+        this.todos = JSON.parse(localStorage.getItem('todos')) || []
+      }
+    
+      bindTodoListChanged(callback) {
+        this.onTodoListChanged = callback
+      }
+    
+      _commit(todos) {
+        this.onTodoListChanged(todos)
+        localStorage.setItem('todos', JSON.stringify(todos))
+      }
+    
+      addTodo(todoText) {
         const todo = {
-            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
-            text: todoText,
-            complete: false
+          id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+          text: todoText,
+          complete: false,
         }
-
+    
         this.todos.push(todo)
-    }
-
-    editTodo(id, updateText) {
-        this.todos = this.todos.map((todo) =>
-            todo.id === id ? { id: todo.id, text: updateText, complete: todo.complete } : todo
+    
+        this._commit(this.todos)
+      }
+    
+      editTodo(id, updatedText) {
+        this.todos = this.todos.map(todo =>
+          todo.id === id ? { id: todo.id, text: updatedText, complete: todo.complete } : todo
         )
-    }
-
-    deleteTodo(id) {
-        this.todos = this.todos.filter((todo) => todo.id !== id)
-    }
-
-    toggleTodo(id) {
-        this.todos = this.todos.map((todo) => 
-            todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo,
+    
+        this._commit(this.todos)
+      }
+    
+      deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+    
+        this._commit(this.todos)
+      }
+    
+      toggleTodo(id) {
+        this.todos = this.todos.map(todo =>
+          todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
         )
-    }
+    
+        this._commit(this.todos)
+      }
 }
 
 
